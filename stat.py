ceb767a2b2fb5af9f3e63692eed076bd4b8bd3b5
vsdeore <deore.venkatesh@gmail.com>
2019-01-26 21:33:48 +0530

    count words

--- stat.py
+++ stat.py
@@ -10,6 +10,19 @@ curr_time = time.time()
 keylog_file='.keylog'
 timelog_file='.timelog'
 
+def decode():
+	fd=open(keylog_file,'r')
+	keys=fd.read()
+	lis=keys.split()
+	print('Number of keystrokes : ')
+	print(len(lis))
+	count=0
+	for i in lis:
+		if i=='space':
+			count=count+1
+	print('Number of words : ')
+	print(count)
+
 def OnKeyPress(event):
 	fob=open(keylog_file,'a')
 	fob.write(event.Key)
@@ -24,28 +37,34 @@ def start_keylogger():
 	new_hook.HookKeyboard()
 	new_hook.start()
 
-try:
-	fptr =open(".timelog" ,"r")
-except:
-	fptr=open(".timelog","w")
-	fptr.write(str(curr_time))
-	print("Stopwatch started at " + str(datetime.datetime.now()))
-	pid=os.fork()
-	if pid>0:
-		print('Parent Process')
+def entry():
+	try:
+		fptr =open(".timelog" ,"r")
+	except:
+		fptr=open(".timelog","w")
+		print('likh rha mai' + str(curr_time))
+		fptr.write(str(curr_time))
+		print("Stopwatch started at " + str(datetime.datetime.now()))
+		pid=os.fork()
+		if pid>0:
+			print('Parent Process')
+		else:
+			id = os.getpid()
+			command = 'echo '+str(id)+' > .pidlog'
+			os.system(command)
+			start_keylogger()
 	else:
-		id = os.getpid()
-		command = 'echo '+str(id)+' > .pidlog'
-		os.system(command)
-		start_keylogger()
-else:
-	if fptr:
-		prev_time=fptr.read()
-		prev_time=float(prev_time)
-		print("Stopwatch ended at " + str(datetime.datetime.now()))
-		print("Worked for ",curr_time-prev_time)
-		os.remove(".timelog")
-		fptr=open(".pidlog","r")
-		id = fptr.read()
-		print(id)
-		os.system('kill '+ id)
+		if fptr:
+			prev_time=fptr.read()
+			prev_time=float(prev_time)
+			print("Stopwatch ended at " + str(datetime.datetime.now()))
+			print("Worked for ",curr_time-prev_time)
+			os.remove(".timelog")
+			fptr=open(".pidlog","r")
+			id = fptr.read()
+			print(id)
+			os.system('kill '+ id)
+			decode()
+			os.remove(".pidlog")
+			os.remove(".keylog")
+entry()
